bbcf32e130f2c09e615da6433ff3ad242885bcab
Jon Bolkema <jrbolkema@gmail.com>
2019-04-09 17:08:41 -0600

    started tag helper

---
+++ RockeyProject/Infrastructure/AboutEmployeeTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Razor.Runtime;
+using RockeyProject.Models;
+
+namespace RockeyProject.TagHelpers
+{
+	// You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+	[HtmlTargetElement("about-employee")]
+	public class AboutEmployeeTagHelper : TagHelper
+	{
+		public Employee Employee { get; set; }
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+
+			output.Content.AppendHtml("<div class='col-2 d-inline-block'>");
+			output.Content.AppendHtml($"<img src='{Employee.Photo}' alt='{Employee.FullName}' height='150' width='150'>");
+			output.Content.AppendHtml($"<p class='m-0'>{Employee.FullName}</p>");
+            output.Content.AppendHtml($"<p class='m-0'>{Employee.Position}</p>");
+            output.Content.AppendHtml($"<p class='m-0'>{Employee.Email}</p>");
+			output.Content.AppendHtml($"<p class='m-0'>{Employee.PhoneNumber}</p>");
+			output.Content.AppendHtml("</div>");
+
+		}
+}
+}
---
+++ RockeyProject/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockeyProject.Models
+{
+	public class Employee
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string FullName { get; set; }
+		public string Email { get;}
+		public string PhoneNumber { get; set; }
+		public string Position { get; set; }
+		public string Photo { get; set; }
+		public Employee(string FName,string LName, string PhoneNumberParamater,string PositionParameter)
+		{
+			FName = FirstName;
+			LName = LastName;
+			Email = $"{FName}.{LName}@Rockeys.com";
+			FullName = $"{FName} {LName}";
+			Photo = $"{FName}.png";
+			PhoneNumberParamater = PhoneNumber;
+			PositionParameter = Position;
+
+		}
+	}
+}
